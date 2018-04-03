ecc62b45b85dff59452a19e82f8584083669f0e9
Diogo Ide <diogoide@gmail.com>
2018-04-03 23:16:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 sentence = ""
 def reverse_each_word(sentence)
   #string to array
-    sentence.split(" ") = sentence_array
-    sentence.each do |word|
+    sentence_array = []
+    sentence.split(" ").each do |word|
     sentence_array << "#{word}".reverse
     end
     #array back to string
